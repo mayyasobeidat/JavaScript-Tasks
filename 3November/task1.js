bf1f722bae18a89c2c40b851f2020151a1615923
15 - Mayyas Obeidat <mayyasobeidat@gmail.com>
2022-11-03 18:08:47 -0800

    new task

---
+++ 3November/task1.js
@@ -0,0 +1,33 @@
+var mark = 84 ;
+if (mark >= 0 && mark <= 49 ) {
+    document.write("F");
+}
+
+else if (mark >= 50&& mark <= 59 ) {
+    document.write("D");
+}
+
+else if (mark>= 60 && mark <= 69 ) {
+    document.write("C");
+}
+
+else if (mark >=70 && mark <= 79){
+    document.write("B");
+}
+else if (mark >= 80 && mark <= 89) {
+    document.write("A");
+}
+ else if (mark >= 90 && mark <= 100) {
+
+    document.write("A+");
+ }
+
+ else  {
+    document.write("wrong value");
+ }
+
+
+
+
+
+
---
+++ 3November/task2.js
@@ -0,0 +1,4 @@
+const x=3.14
+var y = 8
+z=(4/3)*x*(y*y*y)
+document.write (z);
